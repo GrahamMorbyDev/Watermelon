85a9447b27f7b6776e865b7e8404f95094912eef
Graham Morby-Raybould <Grahammorbydev@gmail.com>
2017-07-14 22:29:20 +0100

    Video upload controller started

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,4 +15,23 @@ class AdminController extends Controller
     public function UploadVideo() {
         return view('admin/uploadvideo');
     }
+
+    // Upload single Video
+    public function UploadSingleVideo() {
+        $title = $_POST['title'];
+        $release = $_POST['realeasedate'];
+        $description = $_POST['videodescription'];
+
+        //image
+        $fileimage = $_POST['featuredimage'];
+        $dirimage = base_path('public/uploads/featuredvideoimages');
+        file_put_contents("$dirimage/$fileimage", $fileimage);
+
+        //video
+        $filevideo = $_POST['videofile'];
+        $dirvideo = base_path('public/uploads/videos');
+        file_put_contents("$dirvideo/$filevideo", $filevideo);
+
+        echo $title . $release . $description;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminController@AdminHome');
 
 Route::get('/admin/uploadvideo' , 'AdminController@UploadVideo');
+Route::post('uploadsinglevideo' , 'AdminController@UploadSingleVideo');
