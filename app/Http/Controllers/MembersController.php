6b651698c9d708f765c9d03ca99fdf2b26035018
Scott <scott.austin@lendy.co.uk>
2017-07-23 19:27:25 +0100

    video pagination again

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Storage;
 class MembersController extends Controller
 {
     public function videos() {
-        $videos = DB::table('videos')->get();
+        $videos = DB::table('videos')->paginate();
 
         return view('members/videoList' , ['videos' => $videos]);
     }
