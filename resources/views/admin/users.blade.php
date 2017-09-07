4b1fb70965e08ffddf398e4787c28a16c0acc4b5
Scott <scott.austin@lendy.co.uk>
2017-09-07 21:19:18 +0100

    if check on address

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -50,7 +50,7 @@
                                     <div class="modal-body" style="color: black;">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                         <input type="hidden" name="id" value="{{ $user->id }}">
-                                        <? $address = false; var_dump($address);  die();?>
+                                        <? $address = $user->getAddress()?>
                                         @if($address)
                                         <div class="form-group">
                                             <label for="name-{{$user->id}}">Name</label>
