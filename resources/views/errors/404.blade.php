3fc598cd2de23d31310c8b47522aa2c455bcd06e
Graham Morby-Raybould <Grahammorbydev@gmail.com>
2017-09-12 06:47:51 +0100

    Changed 404 error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,14 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 fourzero">
-                <div class="col-md-6">
-                    <img src="{{URL::asset('images/404.png')}}" alt="404 Page not found" class="img-responsive">
-                </div>
-                <div class="col-md-6">
-                    <h1>404 <br><span>Page not found</span></h1>
-                    <p>Oh God No! Not again? Please look away nothing to see here</p>
-                    <p>Just go back the way you came and try again once we clean this mess up</p>
-                </div>
+                <img src="{{URL::asset('images/404-1.png')}}" alt="404 Page not found" class="img-responsive">
             </div>
         </div>
     </div>
