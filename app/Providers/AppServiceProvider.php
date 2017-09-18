d7a54e5dca5ddcf6847efac4595302b4f0de3910
Scott <scott.austin@lendy.co.uk>
2017-09-18 18:01:18 +0100

    fixed issue where config was required even when artisan was run

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,8 +16,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-//        $config = new Config();
-//        $config->getEnabled();
+        $config = new Config();
+        $config->getEnabled();
 
     }
 
