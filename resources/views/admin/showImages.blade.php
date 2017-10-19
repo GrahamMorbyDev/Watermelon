46b02d8310193c1b7f4f3995bfed52e7e3ae9feb
Graham Morby-Raybould <Grahammorbydev@gmail.com>
2017-10-19 18:53:21 +0100

    moved text

--- resources/views/admin/showImages.blade.php
+++ resources/views/admin/showImages.blade.php
@@ -7,7 +7,7 @@
 
         @foreach($images as $image)
                 <div class="col-md-3 text-center thumbnail">
-                    <h1>Set Name: {{$image->setname}}</h1>
+                    <p>Set Name: {{$image->setname}}</p>
                     <img src="{{URL::asset("storage/" . $image->name)}}" alt="" class="img-responsive" style="min-height: 300px;">
                     <hr>
                     <form action="{{URL::asset('/admin/images/delete')}}" method="post" class="form-group">
