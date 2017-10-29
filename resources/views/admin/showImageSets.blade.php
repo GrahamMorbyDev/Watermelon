d68ee7ba2cdaefed04b7d3386dfcaeaca5d19018
Graham morby Raybould <lordgreymaul@gmail.com>
2017-10-29 18:32:17 +0000

    added image sets
    Updated imaged sets

--- resources/views/admin/showImageSets.blade.php
+++ resources/views/admin/showImageSets.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts/admin')
 @section('content')
     <div class="row">
-        <h1>Show Images</h1>
+        <h1>Show Image Sets</h1>
     </div>
     <div class="row">
         <table class="table table-bordered">
             <thead>
             <tr>
-                <td>ID</td>
                 <td>Name</td>
                 <td>Featured Image</td>
                 <td>Created</td>
@@ -19,19 +18,18 @@
             <tbody>
             @foreach($images as $image)
                 <tr>
-                    <td>{{$image->id}}</td>
                     <td>{{$image->setname}}</td>
                     <td><img src="{{URL::asset("storage/" . $image->name)}}" alt="" width="100px"></td>
                     <td>{{$image->created_at}}</td>
                     <td>{{$image->updated_at}}</td>
                     <td>
-                        <a href="{{URL::asset('/admin/images/'.$image->id)}}" type="submit" class="btn btn-success">Edit</a>
+                        <a href="{{URL::asset('/admin/images/set/'.$image->setname)}}" type="submit" class="btn btn-success">View Set</a>
                     </td>
                     <td>
-                        <form action="{{URL::asset('/admin/images/delete')}}" method="post" class="form-group">
+                        <form action="{{URL::asset('/admin/images/set/delete')}}" method="post" class="form-group">
                             <button class="btn btn-warning">Delete</button>
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <input type="hidden" name="id" value="{{$image->id}}">
+                            <input type="hidden" name="setname" value="{{$image->setname}}">
                         </form>
                     </td>
                 </tr>
