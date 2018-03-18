66e2abc53506a6699f3d6f91b0c5462688ca152d
Graham Morby-Raybould <Grahammorbydev@gmail.com>
2018-03-18 17:49:01 +0000

    fixed up video page

--- resources/views/members/videoList.blade.php
+++ resources/views/members/videoList.blade.php
@@ -5,10 +5,14 @@
         <div class="col-lg-12">
             <h1>Videos</h1>
             @foreach($videos as $video)
-                <div class="col-md-4 text-center" style="min-height: 525px;">
-                    <h3>{{$video->title}}</h3>
-                    <img src="{{URL::asset("storage/" . $video->featuredimage)}}" alt="" class="img-responsive">
-                    <hr>
+                <div class="col-md-4 text-center" style="min-height: 430px;">
+                    <div style="height: 75px;">
+                        <h3>{{$video->title}}</h3>
+                    </div>
+                    <div style="height: 150px; overflow: hidden;">
+                        <img src="{{URL::asset("storage/" . $video->featuredimage)}}" alt="" class="img-responsive">
+                    </div>
+                        <hr>
                     <a href="{{URL::asset('members/video/' . $video->id)}}" class="btn btn-success center-block">Watch Now</a>
                     <br>
                     <form action="{{URL::asset('members/video/download')}}" method="post">
@@ -17,6 +21,7 @@
                         <input type="hidden" name="title" id="title" value="{{$video->title}}">
                         <button type="submit" class="btn btn-warning center-block" style="width: 100%">Download</button>
                     </form>
+                    <hr>
                 </div>
             @endforeach
         </div>
