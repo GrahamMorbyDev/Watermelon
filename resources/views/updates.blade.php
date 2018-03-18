7a560236e6e86b45cde0822466a4b8bde9ae3bc7
Graham Morby-Raybould <Grahammorbydev@gmail.com>
2018-03-18 18:01:46 +0000

    moved links and added margin to image title

--- resources/views/updates.blade.php
+++ resources/views/updates.blade.php
@@ -32,7 +32,9 @@
                     </div>
                 @endforeach
             </div>
-            {{$videos->links()}}
+            <div class="col-md-12 text-center">
+                {{$videos->links()}}
+            </div>
         </div>
         <div class="row">
             <div class="col-lg-12">
@@ -40,7 +42,7 @@
                 <h1>Images</h1>
                 @foreach($images as $image)
                     <div class="col-md-4 text-center">
-                        <div style="height: 55px">
+                        <div style="height: 55px; margin-bottom: 5px;">
                             <h2>{{$image->setname}}</h2>
                         </div>
                         <div style="height: 150px; overflow: hidden">
