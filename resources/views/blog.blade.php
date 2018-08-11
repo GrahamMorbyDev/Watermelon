97309c1cbc72ca85421d3c87e9a8b174ca0e8541
Graham Morby <>
2018-08-11 14:07:52 -0700

    colors and views

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <?$auth = \Illuminate\Support\Facades\Auth::user()?>
+            <?php $auth = \Illuminate\Support\Facades\Auth::user() ?>
             @if(isset($auth) && $auth->isAdmin())
                 <button class="btn btn-success" type="button" onclick="window.location='{{ URL::asset("/admin/blog/".$blog->id) }}'" >Edit Blog</button>
             @endif
-            <div class="col-lg-12">
+            <div class="col-lg-12 blogview">
                 <h1>{{$blog->title}}</h1>
                 <h3>{{$blog->description}}</h3>
                 <img src="{{URL::asset("storage/" . $blog->featuredimage)}}" alt="" class="img-responsive">
