96a0ff6a6e7c641bb40cd5d34b665c3e89d264e1
Graham Morby <>
2018-08-29 12:30:37 -0700

    changed blog body

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -11,7 +11,7 @@
                 <h1>{{$blog->title}}</h1>
                 <h3>{{$blog->description}}</h3>
                 <img src="{{URL::asset("storage/" . $blog->featuredimage)}}" alt="" class="img-responsive">
-                <p>{!!html_entity_decode($blog->body)!!}</p>
+                <p>{!!$blog->body!!}</p>
                 <p>By {{$blog->author}} {{$blog->updated_at}} </p>
             </div>
         </div>
